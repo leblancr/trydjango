7ba5ea1ca81af6381ebb9df54f4e1048197216fe
leblancr <rich.leblanc1@outlook.com>
2023-09-19 15:02:22 -0400

    getting product details from database

--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+# Product is the database
+def product_detail_view(request):
+    obj = Product.objects.get(id=1)
+    # context = {
+    #     'title': obj.title,
+    #     'description': obj.description
+    # }
+    context = {
+        'object': obj
+    }
+    return render(request, 'product/details.html', context)
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -17,9 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from pages.views import home_view, contact_view, about_view
+from products.views import product_detail_view
 
+#  different functions in different files
 urlpatterns = [
     path('', home_view, name='home'),
+    path('product/', product_detail_view),
     path('contact/', contact_view, name='contact'),
     path('about/', about_view, name='about'),
     path('admin/', admin.site.urls),
