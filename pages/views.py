2cb32f353d70d00c0610745d8ce5c218d0e6290b
leblancr <rich.leblanc1@outlook.com>
2023-09-19 10:10:07 -0400

    added template context passing variables

--- pages/views.py
+++ pages/views.py
@@ -10,4 +10,7 @@ def contact_view(request, *args, **kwargs):
     return render(request, "contact.html", {})
 
 def about_view(request, *args, **kwargs):
-    return render(request, "about.html", {})
+    my_context = {'my_text': 'about function',
+                  'my_number': 123
+                  }
+    return render(request, "about.html", my_context)
